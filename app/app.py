c92b5b3c901d2e8795efc96e5f7f21c537ac9a14
tamil-eniyan <eniyan2001@gmail.com>
2024-03-11 15:00:38 +0530

    Layout update

--- app/app.py
+++ app/app.py
@@ -15,34 +15,33 @@ app.resizable(0,0)
 
 
 
-frame1 = tk.LabelFrame(app,text="Data")
-frame1.pack(height=400,width=300)
-
-#title_lable = tk.Label(master = app ,text = "Pre Procssing")
-#title_lable.pack()
-
+frame1 = tk.LabelFrame(app,text="Data",height=300,width=400 )
 command_frame = tk.LabelFrame(app,text="Commands")
-command_frame.pack(side="bottom",fill="both")
-
-encoder_frame = tk.LabelFrame(app,text="Encoders")
-encoder_frame.pack(side="bottom",fill="both")
-
-
-file_frame = tk.LabelFrame(app,text="Open File")
-file_frame.pack(side="bottom",fill="both")
+encoder_frame = tk.LabelFrame(app,text="Encoders",height=400,width=200)
+file_frame = tk.LabelFrame(app,text="File Management",height=100,width=400)
 
 
 
-button1 = tk.Button(file_frame,text="Browse A File",command=lambda: File_dialog())
-button1.place(relx=0.65,rely=0.50)
+def do_layout():
+    frame1.pack(side="left", fill="both", expand=True)
+    encoder_frame.pack(side="top", fill="both", expand=True)
+    file_frame.pack(side="bottom", fill="both", expand=True)
+    return None
 
 
-button2 = tk.Button(file_frame,text="Load File",command=lambda:Load_csv_data())
-button2.place(relx=0.65,rely=0.20)
+do_layout()
+
+#buttons at File Management
+browse_button = tk.Button(file_frame,text="Browse The File",command=lambda: File_dialog())
+browse_button.place(relx=0.10,rely=0.40)
 
 
-lable_file=ttk.Label(file_frame,text="No File Selected")
-lable_file.place(rely=0,relx=0)
+loadfile_button = tk.Button(file_frame,text="   Load File   ",command=lambda:Load_csv_data())
+loadfile_button.place(relx=0.10,rely=0.60)
+
+
+lable_file=ttk.Label(file_frame,text="[-]No File Selected")
+lable_file.place(relx=0.10,rely=0.10)
 
 
 
@@ -64,7 +63,7 @@ def File_dialog():
     filename = filedialog.askopenfilename(initialdir="/home/tamil/Documents/",
                                           title="Select A File",
                                           filetypes=(("csv files","*.csv"),("All Files","*.*")))
-    lable_file["text"] = filename
+    lable_file["text"] = f"{filename}"
     return None
 
 def Load_csv_data():
